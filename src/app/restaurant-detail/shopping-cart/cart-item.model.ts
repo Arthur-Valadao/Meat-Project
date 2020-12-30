ee41e99cf6cf105cfcb07706f42f1914b3969a14
Arthur Valadao <agv.eng16@uea.edu.br>
2020-12-30 14:04:43 -0400

    inserção carrinho e menu

---
+++ src/app/restaurant-detail/shopping-cart/cart-item.model.ts
@@ -0,0 +1,11 @@
+import { MenuItem } from "./../menu-item/menu-item.model";
+
+export class CartItem {
+  constructor(public MenuItem: MenuItem,
+              public quantity: number = 1) {}
+
+  value(): number {
+    return this.MenuItem.price * this.quantity
+  }
+}
+
